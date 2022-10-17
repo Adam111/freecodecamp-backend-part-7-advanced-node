aa562aa65c42ed17275ad5a10f8baf622b95b89c
Adam777 <Adam777-5867@outlook.com>
2022-10-17 17:42:24 +0000

    Update

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const express    = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const mongo      = require('mongodb').MongoClient;
+const mongo = require('mongodb').MongoClient;
 
-const auth   = require('./auth.js');
+const auth = require('./auth.js');
 const routes = require('./routes.js');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+fccTesting(app); // For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,24 +18,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
 
 mongo.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
-  if (err) {
-    console.log('Database error: ' + err);
-  } else {
-    console.log('Successful database connection');
-    
-    auth(app, db);
-    routes(app, db);
-    
-    // app.listen
-    const listener = app.listen(process.env.PORT || 3000, () => {
-      console.log('Listening on port ' + listener.address().port);
-    });
-  }
+	if (err) {
+		console.log('Database error: ' + err);
+	} else {
+		console.log('Successful database connection');
+
+		auth(app, db);
+		routes(app, db);
+
+		// app.listen
+		const listener = app.listen(process.env.PORT || 3000, () => {
+			console.log('Listening on port ' + listener.address().port);
+		});
+	}
 });
 
 // Enables to pass the challenge called "Advanced Node and Express - Registration of New Users"
 if (process.env.ENABLE_DELAYS === 'true') {
-  app.use((req, res, next) => {
-    return setTimeout(() => next(), 500);
-  });
+	app.use((req, res, next) => {
+		return setTimeout(() => next(), 500);
+	});
 }
